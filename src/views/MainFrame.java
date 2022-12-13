b1829a8f62f7bfcbc10cea3c382196711b37e01f
csengekulker <k71608883249@ktch.hu>
2022-12-13 13:42:58 +0100

    components positioning

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -1,30 +1,82 @@
 package views;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import lan.bcs.InputPanel;
+
+import java.awt.GridLayout;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 
 public class MainFrame extends JFrame {
   JPanel mainPanel;
 
+  JPanel northPanel;
+    JLabel mainLabel;
+
+  JPanel centerPanel;
+    InputPanel sideInput;
+    InputPanel angleInput;
+    JButton submitButton;
+    InputPanel perimeter;
+    InputPanel area;
+
+  JPanel southPanel;
+    JButton exitButton;
+    JButton creditsButton;
+
   public MainFrame() {
 
-    setComponents();
     setMainPanel();
+    setComponents();
     setFrame();
   }
 
   private void setComponents() {
 
+    northPanel = new JPanel();
+      mainLabel = new JLabel("Rombusz adatai");
+
+    centerPanel = new JPanel(new GridLayout(5, 1));
+
+      sideInput = new InputPanel();
+      sideInput.setText("A oldal:");
+      angleInput = new InputPanel();
+      angleInput.setText(("Alpha: "));
+
+      submitButton = new JButton("Számítás");
+
+      perimeter = new InputPanel();
+      perimeter.setText("Kerület: ");
+      area = new InputPanel();
+      area.setText("Terület: ");
+
+    southPanel = new JPanel(new GridLayout(1, 2));
+
+      exitButton = new JButton("Kilépés");
+      creditsButton = new JButton("Névjegy");
+
+    northPanel.add(mainLabel);
+
+    centerPanel.add(sideInput);
+    centerPanel.add(angleInput);
+    centerPanel.add(submitButton);
+    centerPanel.add(perimeter);
+    centerPanel.add(area);
+
+    southPanel.add(creditsButton);
+    southPanel.add(exitButton);
+
+    mainPanel.add(northPanel, BorderLayout.NORTH);
+    mainPanel.add(centerPanel, BorderLayout.CENTER);
+    mainPanel.add(southPanel, BorderLayout.SOUTH);
+
   }
 
   private void setMainPanel() {
-    this.mainPanel = new JPanel();
-
-    this.mainPanel.setLayout(
-      new BorderLayout()
-    );
+    this.mainPanel = new JPanel(new BorderLayout());
 
     this.add(this.mainPanel);
   }
@@ -32,14 +84,8 @@ public class MainFrame extends JFrame {
   private void setFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(400, 300);
-    this.setTitle("JAVA GUI TEMPLATE");
+    this.setTitle("Rombusz");
     this.setVisible(true);
 
-    // layout for window
-    this.setLayout(
-      new FlowLayout(
-
-      )
-    );
   }
 }
